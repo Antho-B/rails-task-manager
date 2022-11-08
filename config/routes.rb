37ddbb037a21e83d270bec3082d4cd5c4432b999
anthonyborremans <anthonyborremans@hotmail.com>
2022-11-08 16:47:05 -0500

    1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,22 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Read all
+  get '/tasks', to: 'tasks#index'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # Create
+  # 1. Provide the form
+  get '/tasks/new', to: 'tasks#new', as: :new_task
+  # 2. Send the form
+  post '/tasks', to: 'tasks#create'
+
+  # Read one
+  get '/tasks/:id', to: 'tasks#show', as: :task
+
+  # Update
+  # 1. Provide the form
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # 2. Send the form
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # Destroy
+  delete '/tasks/:id', to: 'tasks#destroy'
 end
